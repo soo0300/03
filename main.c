c702b15e4aa0c33b01004d8fa61e41ddf1e04912
soo0300 <soo0300@naver.com>
2022-09-21 17:31:13 +0900

    03_first

---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void) {
+	//½Ç½À1
+	int input_int;
+	float input_float;
+	
+	printf("write an integer: ");
+	scanf("%d", &input_int);
+
+	printf("write a float: ");
+	scanf("%f", &input_float);
+
+	printf("integer: %d, float: %f \n", input_int, input_float);
+	
+	return 0;
+}
+
+
